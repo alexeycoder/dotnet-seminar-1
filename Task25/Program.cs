45e44a2b0d41da27a9a3707726a01ced1f99353d
Alexey Kuznetsov <leoxander@yandex.ru>
2022-08-23 14:30:08 +0300

    Added 6th seminar tasks

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+int[] sides = new int[3];
+
+Console.WriteLine("Задайте длины сторон предполагаемого треугольника:");
+for (int i = 0; i < sides.Length; ++i)
+{
+	Console.Write($"Длина {i + 1}-й стороны: ");
+	sides[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+bool isValid = IsValidTriangle(sides);
+
+Console.WriteLine(isValid ? "Треугольник с такими сторонами может существовать." : "Невозможно существование треугольнака с заданными сторонами.");
+
+// Methods
+
+static bool IsValidTriangle(int[] lengths)
+{
+	int maxLength = lengths[0];
+	if (lengths[1] > maxLength) maxLength = lengths[1];
+	if (lengths[2] > maxLength) maxLength = lengths[2];
+
+	return lengths[0] + lengths[1] + lengths[2] - maxLength > maxLength;
+}
---
+++ Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5-> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintFibonacciSequence(n);
+
+// Methods
+
+void PrintFibonacciSequence(int n)
+{
+	int prevPrev = 0;
+	int prev = 1;
+
+	Console.Write($"{prevPrev} {prev} ");
+
+	for (int i = 3; i <= n; ++i)
+	{
+		int fibo = prev + prevPrev;
+		Console.Write(fibo + " ");
+
+		prevPrev = prev;
+		prev = fibo;
+	}
+
+	Console.WriteLine();
+}
