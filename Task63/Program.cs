6727be4317e053b8ddd11a1afd62c18772ec2a88
Alexey Kuznetsov <leoxander@yandex.ru>
2022-09-02 12:12:40 +0300

    Added 9th seminar tasks

---
+++ Task63/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumber(number);
+Console.WriteLine();
+NaturalNumberReverseOrder(number);
+Console.WriteLine();
+
+static void NaturalNumber(int num)
+{
+	if (num == 0)
+		return;
+
+	NaturalNumber(num - 1);
+	Console.Write($"{num} ");
+}
+
+static void NaturalNumberReverseOrder(int num)
+{
+	if (num == 0)
+		return;
+
+	Console.Write($"{num} ");
+	NaturalNumber(num - 1);
+}
---
+++ Task65/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите целое положительное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalisV2(m, n);
+
+Console.WriteLine();
+
+static void PrintNaturalis(int m, int n)
+{
+	Console.Write($"{m} ");
+
+	if (m > n)
+		PrintNaturalis(m - 1, n);
+	else if (m < n)
+		PrintNaturalis(m + 1, n);
+}
+
+static void PrintNaturalisV2(int m, int n)
+{
+	if (m == n)
+	{
+		Console.Write($"{m}");
+		return;
+	}
+	Console.Write($"{m}, ");
+
+	if (m > n)
+		PrintNaturalisV2(m - 1, n);
+	else if (m < n)
+		PrintNaturalisV2(m + 1, n);
+}
